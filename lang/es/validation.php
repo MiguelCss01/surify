6df3d10ca9c03dee89be42d2ed922bec9ce60949
Miguel Casasola <miguelcasasola37@gmail.com>
2026-06-06 18:51:05 -0300

    intl: traduccion completa de laravel breeze y validaciones al espanol

---
+++ lang/es/validation.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+    'accepted' => 'El campo :attribute debe ser aceptado.',
+    'confirmed' => 'La confirmación de :attribute no coincide.',
+    'email' => 'El campo :attribute debe ser una dirección de correo válida.',
+    'min' => [
+        'numeric' => 'El campo :attribute debe ser al menos :min.',
+        'file' => 'El campo :attribute debe pesar al menos :min kilobytes.',
+        'string' => 'El campo :attribute debe contener al menos :min caracteres.',
+        'array' => 'El campo :attribute debe tener al menos :min elementos.',
+    ],
+    'required' => 'El campo :attribute es obligatorio.',
+    'unique' => 'El campo :attribute ya ha sido registrado.',
+    
+    'custom' => [],
+
+    'attributes' => [
+        'name' => 'nombre',
+        'email' => 'correo electrónico',
+        'password' => 'contraseña',
+        'password_confirmation' => 'confirmar contraseña',
+        'calificacion' => 'calificación',
+        'comentario' => 'comentario',
+    ],
+];
